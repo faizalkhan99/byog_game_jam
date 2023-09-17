01a9b62a7998d3d245b6cf92969db50b789128b3
Faizal Khan <fk624178@gmail.com>
2023-09-17 19:21:02 +0530

    commit

---
+++ Assets/Pixel Art Top Down - Basic/love.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class love : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _love;
+    void Start()
+    {
+        _love.text = "";
+        StartCoroutine(Lovetxt());
+    }
+
+    public IEnumerator Lovetxt()
+    {
+        yield return new WaitForSeconds(2.0f);
+        _love.text = "OF LOVE <3";
+    }
+
+}
