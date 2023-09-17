ae6a599be9aaaba343c0af888fe859edea3c7b50
Kavyansh Barjattya <117171893+kavyanshbarjattya@users.noreply.github.com>
2023-09-17 13:18:51 +0530

    LoadingSystem bna diya

---
+++ Assets/Kavyansh/Scripts/MainMenu.cs
@@ -0,0 +1,57 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] GameObject _creditsScreen , _mainmenuScreen , _loadingScreen;
+    [SerializeField] Slider _loader;
+    [SerializeField] TextMeshProUGUI _loading_txt;
+
+    private void Start()
+    {
+        _mainmenuScreen.SetActive(true);
+        _creditsScreen.SetActive(false);
+        _loadingScreen.SetActive(false);
+    }
+    #region Main menu
+    public void Play(string Level)
+    {
+        _mainmenuScreen.SetActive(false);
+        _loadingScreen.SetActive(true);
+        StartCoroutine(SceneLoader(Level));
+    }
+
+    public void Credits()
+    {
+        _mainmenuScreen.SetActive(false);
+        _creditsScreen.SetActive(true);
+    }
+    public void Back_Button()
+    {
+        _creditsScreen.SetActive(false);
+        _mainmenuScreen.SetActive(true);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+
+    IEnumerator SceneLoader(string Level)
+    {
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(Level);
+
+        while (!asyncOperation.isDone)
+        {
+            float progress = Mathf.Clamp01(asyncOperation.progress / 0.9f);
+            _loader.value = progress;
+            _loading_txt.text  = (int)progress * 100f + " %";
+            yield return null;
+        }
+    }
+    #endregion
+}
